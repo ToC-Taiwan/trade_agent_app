257e343375c5abbe3c1bf5f5142f2904e58195a8
Tim Hsu@M2MAX-20230327 <maochindada@gmail.com>
2023-04-01 00:04:26 +0800

    chore(update): update dependency

--- scripts/flutter_clean.sh
+++ scripts/flutter_clean.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-flutter pub cache clean
 flutter clean
 flutter pub get
 
-echo 'storePassword=2@ZdP-V&Z7xNBwc!wdNDSquH
-keyPassword=2@ZdP-V&Z7xNBwc!wdNDSquH
-keyAlias=play_console_upload
-storeFile=/Users/timhsu/dev_projects/key/upload-keystore.jks' > ./android/key.properties
+echo 'storePassword=2@ZdP-V&Z7xNBwc!wdNDSquH' >./android/key.properties
+echo 'keyPassword=2@ZdP-V&Z7xNBwc!wdNDSquH' >>./android/key.properties
+echo 'keyAlias=play_console_upload' >>./android/key.properties
+echo "storeFile=$HOME/android_key/upload-keystore.jks" >>./android/key.properties
 
 # genky path: /Applications/Android Studio.app/Contents/jre/Contents/Home/bin
 # ./keytool -genkey -v -keystore /Users/timhsu/dev_projects/key/upload-keystore.jks -keyalg RSA -keysize 2048 -validity 10000 -alias play_console_upload
